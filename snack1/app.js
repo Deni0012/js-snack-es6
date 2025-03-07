1a29fabe9ecafed48dca7d948acb2767c8d022d1
Deni0012 <deni.dg00@icloud.com>
2025-03-07 15:31:20 +0100

    feat: creazione di const bikes

--- snack1/app.js
+++ snack1/app.js
@@ -1,3 +1,10 @@
 //Creare un array di oggetti:
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore.
+
+const bikes = [
+    { nome: "a", peso: 200 },
+    { nome: "b", peso: 35 },
+    { nome: "c", peso: 100 },
+    { nome: "d", peso: 45 }
+]
