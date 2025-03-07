208105f0f934864b3464830fbeb4a1f073470f7d
Deni0012 <deni.dg00@icloud.com>
2025-03-07 15:30:07 +0100

    feat: aggiunta di commenti su cosa fare

--- snack1/app.js
+++ snack1/app.js
@@ -1,1 +1,3 @@
-console.log('ciao Denise')
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare a schermo la bici con peso minore.
