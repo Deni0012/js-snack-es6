133bf714b7d5c07bd5074c8438b34218440576d8
Deni0012 <deni.dg00@icloud.com>
2025-03-07 16:07:22 +0100

    feat: creazione squadre

--- snack2/app.js
+++ snack2/app.js
@@ -3,3 +3,11 @@
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 //Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const seriea = [
+    { name: "Inter", score: 0, fouls: 0 },
+    { name: "Milan", score: 0, fouls: 0 },
+    { name: "Atalanta", score: 0, fouls: 0 },
+    { name: "Lazio", score: 0, fouls: 0 },
+    { name: "Fiorentina", score: 0, fouls: 0 }
+]
